47054cafe4eb4e2d64e49e49075a75fe0e043dab
zjh336 <806073526@qq.com>
2022-12-15 11:03:46 +0800

    1、布局分析，节点点击优化，更改为点击行不展开，点击箭头才展开。 2、布局分析，绘制框高亮节点点击优化，点击后左侧树自动展开当前选中节点。 2、布局分析，绘制框高亮节点点击优化，点击后左侧树自动定位到当前选中节点，未展开的节点，可能需要点击第二次才能正常定位。

--- src/main/resources/static/module/index/component/layoutAnalysis.js
+++ src/main/resources/static/module/index/component/layoutAnalysis.js
@@ -44,6 +44,7 @@ export default {
                     openCustomFilterFunction:false,// 开启自定义过滤函数处理
                     nodeKeyArr: [],// key数组
                     selectNode: {},
+                    defaultExpandedKeys:[],// 默认展开节点key数组
                     rectCanvasArr: [],// 绘制方框数组
                     highlightRect: null,// 高亮方框
                     dialogVisible: false,
@@ -559,6 +560,17 @@ export default {
                 this.remoteHandler.param5.dialogVisible = true;
                 // 更改选中树节点
                 this.$refs.nodeTree.setCurrentKey(highlightRect.nodeKey);
+
+                // 默认展开keys
+                this.defaultExpandedKeys = [highlightRect.nodeKey];
+
+                this.$nextTick(()=>{
+                    // 获取高亮节点
+                    let highlightNode = $('.el-tree--highlight-current .el-tree-node.is-current>.el-tree-node__content');
+                    if(highlightNode && highlightNode.length){
+                        highlightNode[0].scrollIntoView();
+                    }
+                });
                 // 点击生成代码
                 this.nodeGenerateCode();
             }
