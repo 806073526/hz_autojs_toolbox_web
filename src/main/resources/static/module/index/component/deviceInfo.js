8dacadf1b4b33b3b2b5732b0ade53a221d8877bf
zjh336 <806073526@qq.com>
2023-11-08 13:22:46 +0800

    3.2.0版本

--- src/main/resources/static/module/index/component/deviceInfo.js
+++ src/main/resources/static/module/index/component/deviceInfo.js
@@ -17,7 +17,7 @@ export default {
     props: {},
     data() {
         return {
-            versionInfo:"V3.1.2",
+            versionInfo:"V3.2.0",
             newVersion:"",
             isExeVersion: false,
             deviceList: [],// 设备列表
